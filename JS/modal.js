8d49d1387ceb32f48d66a8333c97318a8e990acf
Cycle9898 <cycle9898@gmail.com>
2023-01-04 15:53:00 +0100

    Issue #1 : add close modal feature (with clear form and animation)

--- JS/modal.js
+++ JS/modal.js
@@ -1,3 +1,12 @@
+// DOM Elements
+
+const modalBg = document.querySelector(".bground");
+const modalBtn = document.querySelectorAll(".modal-btn");
+const closeModalBtn = document.querySelector(".close");
+const formData = document.querySelectorAll(".formData");
+
+// Change class name (restyle) on burger menu (mobile)
+
 function editNav() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
@@ -7,17 +16,31 @@ function editNav() {
   }
 }
 
-// DOM Elements
-const modalBg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
+// Launch modal event
 
-// launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
-// launch modal form
+// Launch modal form
+
 function launchModal() {
   modalBg.style.display = "block";
 }
 
+// Function to clear form fields
 
+function clearForm(id) {
+  document.getElementById(id).reset();
+}
+
+// Function to clear form and close modal with an animation
+
+function closeModal() {
+  clearForm("modal-form");
+  modalBg.style.animation = "modalopen var(--modal-duration) reverse";
+  setTimeout(() => { modalBg.style.animation = "" }, 800);
+  setTimeout(() => { modalBg.style.display = "none" }, 800);
+}
+
+// Close modal event
+
+closeModalBtn.addEventListener("click", closeModal);
